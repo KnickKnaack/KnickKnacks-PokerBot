4ea76465fc6b2645ce76426d4121dd58046f99ed
KnickKnaack <nicholasgrijalva01@gmail.com>
2025-07-19 14:25:09 -0600

    Flush odds updates

--- TheDestroyer.py
+++ TheDestroyer.py
@@ -4,6 +4,7 @@ import random
 import time
 from collections import Counter
 import math
+import itertools
 
 
 # Import possible actions
@@ -75,50 +76,50 @@ class PokerBot(multiprocessing.Process):
         # but basically if hand has OK odds or has a decent hand already, raise, otherwise call.
         # if hand has low odds and doesn't already have a ok hand, fold if bet was raised at all.
 
-        if draws_left <= 2:
-            score = evaluate_hand(board + hand)[0][0]
-            if score > 2:
-                if score > 4:
-                    # print("has good hand")
-                    if curr_bet < player_curr_chips // 2:
-                        return RaiseAction(player_curr_chips // 2)
-                    return CallAction()
-                # print("has ok hand")
-                if int(curr_bet * 0.5) < player_curr_chips // 2:
-                    RaiseAction(int(curr_bet * 0.5))
-                return CallAction()
-            # print(f"in last draws, score = {score}, draws = {draws_left}")
-            if draws_left == 0 and score >= 1:
-                return CallAction()
+        # if draws_left <= 2:
+        #     score = evaluate_hand(board + hand)[0][0]
+        #     if score > 2:
+        #         if score > 4:
+        #             # print("has good hand")
+        #             if curr_bet < player_curr_chips // 2:
+        #                 return RaiseAction(player_curr_chips // 2)
+        #             return CallAction()
+        #         # print("has ok hand")
+        #         if int(curr_bet * 0.5) < player_curr_chips // 2:
+        #             RaiseAction(int(curr_bet * 0.5))
+        #         return CallAction()
+        #     # print(f"in last draws, score = {score}, draws = {draws_left}")
+        #     if draws_left == 0 and score >= 1:
+        #         return CallAction()
 
-        if player_curr_chips < ante * 2:
-            # print("desperate all in")
-            # print(f"current stack: {player_stack}, ante: {ante}")
-            return CallAction()
-        if      (
-                    flush_odds(hand, board) <= 0.5
-                    and three_odds(hand, board) <= 0.4
-                    and draws_left <= 1
-                ):
-            # print("bad odds")
-            if curr_bet - player_curr_bet == 0:
-                return CallAction()
-            return FoldAction()
-        if (
-                flush_odds(hand, board) >= 0.5
-                or three_odds(hand, board) >= 0.6
-                or quad_odds(hand, board) >= 0.2
-            ):
-            # print("good odds")
-            if player_curr_chips // 10 > curr_bet:
-                return RaiseAction(player_curr_chips // 10)
-            if player_curr_chips > curr_bet - player_curr_bet:
-                return CallAction()
-        if curr_bet - player_curr_bet <= player_curr_chips // 10 or draws_left >= 2:
-            # print("small bet, calling")
-            return CallAction()
-        # print(f"curr_bet: {curr_bet}, player_curr_bet: {player_curr_bet}")
-        # print("############### Bot is confuzed")
+        # if player_curr_chips < ante * 2:
+        #     # print("desperate all in")
+        #     # print(f"current stack: {player_stack}, ante: {ante}")
+        #     return CallAction()
+        # if      (
+        #             flush_odds(hand, board) <= 0.5
+        #             and three_odds(hand, board) <= 0.4
+        #             and draws_left <= 1
+        #         ):
+        #     # print("bad odds")
+        #     if curr_bet - player_curr_bet == 0:
+        #         return CallAction()
+        #     return FoldAction()
+        # if (
+        #         flush_odds(hand, board) >= 0.5
+        #         or three_odds(hand, board) >= 0.6
+        #         or quad_odds(hand, board) >= 0.2
+        #     ):
+        #     # print("good odds")
+        #     if player_curr_chips // 10 > curr_bet:
+        #         return RaiseAction(player_curr_chips // 10)
+        #     if player_curr_chips > curr_bet - player_curr_bet:
+        #         return CallAction()
+        # if curr_bet - player_curr_bet <= player_curr_chips // 10 or draws_left >= 2:
+        #     # print("small bet, calling")
+        #     return CallAction()
+        # # print(f"curr_bet: {curr_bet}, player_curr_bet: {player_curr_bet}")
+        # # print("############### Bot is confuzed")
         return FoldAction()
 
     def end_game(self, game_state_json):
@@ -130,10 +131,14 @@ class PokerBot(multiprocessing.Process):
 
 
 class PokerProbabilities():
-    cardsInDeck = Deck().cards
+    #Constants
     TOTAL_GAME_CARDS = 7
+    FLUSH_CARDS_NEEDED = 5
+
+    cardsInDeck = Deck().cards
     leftToDraw = TOTAL_GAME_CARDS
     currCards = []
+
 
     def nCr(self, NR):
         n, r = NR
@@ -146,9 +151,20 @@ class PokerProbabilities():
     
         return sum
 
-    def evaluate_combinations(self, choices):
-        probability = math.prod(list(map(self.nCr, choices))) / (self.nCr((len(self.cardsInDeck), self.leftToDraw))) 
+    def evaluate_combinations(self, choices, raw = None):
+        if raw == None:
+            total = (self.nCr((len(self.cardsInDeck), self.leftToDraw)))
+        else:
+            total = self.nCr(raw) 
 
+        if (len(choices) <= 0):
+            return 0
+        elif (type(choices[0]) == list):
+            probability = sum(map(lambda NRs : math.prod(map(self.nCr, NRs)), choices)) / total
+        else:
+            probability = math.prod(map(self.nCr, choices)) / total
+
+    
         return probability
         
 
@@ -158,111 +174,92 @@ class PokerProbabilities():
         self.leftToDraw = self.TOTAL_GAME_CARDS
         self.currCards = []
 
-    def add_to_hand(self, toAdd):
+    def take_from_deck(self, toTake, addToHand=True):
         d = self.cardsInDeck
-        for c in toAdd:
+        for c in toTake:
             d.remove(c)
 
-        self.currCards += toAdd
+        if (addToHand):
+            self.currCards += toTake
 
-        self.leftToDraw = self.leftToDraw - len(toAdd)
+            self.leftToDraw = self.leftToDraw - len(toTake)
 
 
     def flush_odds(self):
+
         suits = [card.suit for card in self.currCards]
         suit_counts = Counter(suits)
-        print('my suit coutns', suit_counts)
 
-        
+
+        for suit in Card.SUIT_MAP.values():
+            if suit not in suit_counts:
+                suit_counts[suit] = 0
+
 
         deck_suits = [card.suit for card in self.cardsInDeck]
         deck_suit_counts = Counter(deck_suits)
-        print('deck suit counts', deck_suit_counts)
 
         chance = 0
 
 
-        
         for suit, count in suit_counts.items():
-            excess = self.leftToDraw - (5 - count)
-            if count == 5:
+            cardsNeeded = (self.FLUSH_CARDS_NEEDED - count)
+            excess = self.leftToDraw - cardsNeeded
+            if count == self.FLUSH_CARDS_NEEDED:
                 return 1
             elif (excess) < 0:
                 continue
 
-            combinations = [(deck_suit_counts[suit], (5 - count)), (len(self.cardsInDeck) - deck_suit_counts[suit], excess)]
+            #NOTE: The probabiliy of each suit is independant of eachother. (this may cause a slightly lower chance than actual in high-card cases)
+            #   -ex: with 17 cards, a flush is gaurented
+            # need to differentiate when you pick different amounts of the suit at hand
+            #TODO: Flip this to inverse of the probabillity of not getting enough cards. (I think is more efficient?) (maybe do that for anything more than the minimum)
+            combinations = [[(deck_suit_counts[suit], cardsNeeded + i), (len(self.cardsInDeck) - deck_suit_counts[suit], excess - i)] for i in range(excess + 1)]
             print(combinations)
-            chance = max(chance, self.evaluate_combinations(combinations)) 
+            # chance = max(chance, self.evaluate_combinations(combinations))
+            chance += self.evaluate_combinations(combinations)
             
 
         return chance
 
-    def straight_odds(self, hand, board):
-        all_cards = hand + board
 
-        max_chance = 0
 
-        return max_chance
 
-    def pair_odds(self, hand, board):
-        all_cards = hand + board
-        ranks = [card.rank for card in all_cards]
-        rank_counts = Counter(ranks)
+def flush_probability_with_hand(current_hand, num_drawn=7, flush_size=5, num_suits=4, cards_per_suit=13):
+    suits = ['H', 'S', 'D', 'C'][:num_suits]
+    suit_counter = Counter(current_hand)
 
-        deck_ranks = [card.rank for card in self.cardsInDeck]
-        deck_rank_counts = Counter(deck_ranks)
+    cards_in_hand = len(current_hand)
+    cards_to_draw = num_drawn - cards_in_hand
+    total_cards_remaining = num_suits * cards_per_suit - cards_in_hand
 
-        for rank, count in rank_counts.items():
-            if count == 2:
-                return 1
+    # If already have flush, guaranteed
+    if any(suit_counter[suit] >= flush_size for suit in suits):
+        return 1.0
 
-        return 0.5
+    total_ways = math.comb(total_cards_remaining, cards_to_draw)
+    flush_prob = 0.0
 
-    def three_odds(self, hand, board):
-        all_cards = hand + board
-        ranks = [card.rank for card in all_cards]
-        rank_counts = Counter(ranks)
+    for suit in suits:
+        current_count = suit_counter[suit]
+        needed = flush_size - current_count
+        if needed > cards_to_draw or needed > cards_per_suit - current_count:
+            continue
 
-        deck_ranks = [card.rank for card in self.cardsInDeck]
-        deck_rank_counts = Counter(deck_ranks)
+        max_possible_from_suit = min(cards_per_suit - current_count, cards_to_draw)
+        ways = 0
 
-        max_rank = []
-        max_count = 0
-        for rank, count in rank_counts.items():
-            if count == 3:
-                return 1
-            if count > max_count:
-                max_count = count
-                max_rank.append(rank)
-            if count == max_count:
-                max_rank.append(rank)
-        odd = 0
-        for rank in max_rank:
-            odd += (deck_rank_counts[rank] / len(self.deck_left)) ** (3 - max_count)
-        return 1 - ((1 - odd) ** self.cardsInDeck)
+        # Sum over the number of cards of the target suit drawn to reach flush
+        for k in range(needed, max_possible_from_suit + 1):
+            ways_suit = math.comb(cards_per_suit - current_count, k)
+            ways_other = math.comb(total_cards_remaining - (cards_per_suit - current_count), cards_to_draw - k)
+            ways += ways_suit * ways_other
 
-    def quad_odds(self, hand, board):
-        all_cards = hand + board
-        ranks = [card.rank for card in all_cards]
-        rank_counts = Counter(ranks)
+        flush_prob += ways / total_ways
 
-        deck_ranks = [card.rank for card in self.deck_left]
-        deck_rank_counts = Counter(deck_ranks)
+    # Cap at 1 in case of numerical floating overrun
+    return min(flush_prob, 1.0)
 
-        max_rank = []
-        max_count = 0
-        for rank, count in rank_counts.items():
-            if count == 4:
-                return 1
-            if count > max_count:
-                max_count = count
-                max_rank.append(rank)
-            if count == max_count:
-                max_rank.append(rank)
-        odd = 0
-        for rank in max_rank:
-            odd += (deck_rank_counts[rank] / len(self.cardsInDeck)) ** (4 - max_count)
-        return 1 - ((1 - odd) ** self.cardsInDeck)
 
 def test():
     
@@ -288,22 +285,27 @@ def test():
 
     deck = Deck()
     deck.shuffle()
-    hand = deck.deal(2)
-    board = deck.deal(3)
+    # hand = deck.deal(2)
+    # board = deck.deal()
+    # print(type([(1, 2), (3, 4)][0]))
+    # print(type([[(1, 2), (3, 4)],[(1, 2), (3, 4)]][0]))
     
     hand = [Card('Hearts', '4'), Card('Hearts', '5'), Card('Hearts', '6'), Card('Hearts', '7'),Card('Spades', '4')]
+    # hand = [Card('Hearts', '4'), Card('Hearts', '5'), Card('Hearts', '6'), Card('Clubs', '7'),Card('Spades', '4')]
+    # hand = []
     board = []
 
-    probs.add_to_hand(hand + board)
+    probs.take_from_deck(hand + board)
+
+
     print(probs.currCards)
-    # print(probs.cardsInDeck)
-    print(probs.flush_odds())
+    print('mine:', probs.flush_odds())
+    current_hand = ['H']
+    print('geeps:', flush_probability_with_hand(current_hand, num_drawn=14))
     
-    print(1 - (38*37)/(47*46))
-    # print(probs.nCr((9, 1)))
-    # print(probs.nCr((10, 2)))
-    # print(probs.evalucate_choice([(9, 1)], 2))
-    # print(probs.evalucate_choice([(10, 2)], 2))
+    print('reg:', 1 - (38*37)/(47*46))
+    print('4 needed:', 1 - (37*36)/(47*46))
+
     
 
 
