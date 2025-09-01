23143fadae0613923c433beb86c5a44f3e5548e6
KnickKnaack <nicholasgrijalva01@gmail.com>
2025-09-01 07:59:32 -0600

    lower max, more variablility?

--- Knicks_Knacker.py
+++ Knicks_Knacker.py
@@ -80,7 +80,7 @@ class PokerBot(multiprocessing.Process):
 
         scoreDifference = score - boardScore
 
-        possibleBet = math.floor((score/self.probs.MAX_SCORE) * player_curr_chips)
+        possibleBet = math.floor((score/(self.probs.MAX_SCORE - sum(self.probs.HAND_WEIGHTS[-2:]))) * player_curr_chips)
 
         if (scoreDifference < 0 and curr_bet != 0):
             return FoldAction()
